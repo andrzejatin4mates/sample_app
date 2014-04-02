f4b6ce686bb6c910b768bd9d4a797dd2c72c3f10
Andrzej Derlacki <andrzej.derlacki@in4mates.com>
2014-04-02 16:10:56 +0200

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -65,10 +65,19 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
     it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 
   describe "signup page" do
@@ -78,24 +87,26 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 
-  describe "signup redirected to home" do
+  describe "signup redirected to user's home" do
     let(:user) { FactoryGirl.create(:user) }
     before {
       sign_in user
       visit signup_path
     }
 
-    it { should have_link('Sign up now!') }
+    it { should have_title('') }
+    it { should have_content(user.name) }
   end
 
-  describe "signin redirected to home" do
+  describe "signin redirected to user's home" do
     let(:user) { FactoryGirl.create(:user) }
     before {
       sign_in user
       visit signin_path
     }
 
-    it { should have_content('This is the home page') }
+    it { should have_title('') }
+    it { should have_content(user.name) }
   end
 
   describe "signup" do
